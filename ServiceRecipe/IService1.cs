009a724411bbe317ec47eb37998ef7afc7195ccb
TEW404 <84270286+tew080@users.noreply.github.com>
2025-03-22 21:32:47 +0700

    Update Comment

--- ServiceRecipe/IService1.cs
+++ ServiceRecipe/IService1.cs
@@ -8,6 +8,20 @@ using System.Text;
 
 namespace ServiceRecipe
 {
+    /*
+     * กำหนด Service เพื่อที่จะสามารถ เรียกใช้จาก ฝั่งของ Client ได้
+     * 
+     * List<ShowAll>ShowAllFoods(); เรียกแสดง รายการอาหารทั้งหมด จาก class ShowAll ที่เก็บ FoodID, FoodName
+     * 
+     * SearchAndUpdate SearchFoodName(string input_foodname); เรียกแสดง รายการอาหาร  จาก class SearchAndUpdate 
+     * ที่เก็บ FoodID, FoodName, RawMaterial, Recipe โดยการรับค่าผ่านตัวแปร
+     * 
+     * void AddDataFood(AddData add_data_food); เพิ่มข้อมูล ใช้ class AddData ที่เก็บ FoodName, RawMaterial, Recipe โดยการรับค่าผ่านตัวแปร เเละจะไม่มีการส่งค่า Return กลับ
+     * 
+     * void UpdateDataFood(SearchAndUpdate update_data_food); อัพเดทข้อมูล ใช้ class SearchAndUpdate ที่เก็บ FoodID, FoodName, RawMaterial, Recipe โดยการรับค่าผ่านตัวแปร เเละจะไม่มีการส่งค่า Return กลับ
+     * 
+     * bool DeleteDataFood(DeleteData delete_data_food); ลบข้อมูล ใช้ class DeleteData  ที่เก็บ FoodName โดยการรับค่าผ่านตัวแปร และมีการส่งค่ากลับ(True/False)
+     */
     [ServiceContract]
     public interface IService1
     {
@@ -26,6 +40,12 @@ namespace ServiceRecipe
         [OperationContract]
         bool DeleteDataFood(DeleteData delete_data_food);
     }
+
+    /*
+     * กำหนด class ShowAll เพื่อเก็บ FoodID เป็น int , FoodName เป็น string 
+     * และสามารถเรียกดูและกำหนดค่าลงไปได้
+     * จะถูกเรียกใช้โดย Service List<ShowAll>ShowAllFoods();
+     */
     public class ShowAll
     {
         [DataMember]
@@ -34,6 +54,12 @@ namespace ServiceRecipe
         [DataMember]
         public string FoodName { get; set; }
     }
+
+    /*
+     * กำหนด class ShowAll เพื่อเก็บ FoodName เป็น string,  RawMaterial เป็น string,  Recipe เป็น string 
+     * และสามารถเรียกดูและกำหนดค่าลงไปได้
+     * จะถูกเรียกใช้โดย Service void AddDataFood(AddData add_data_food);
+     */
     public class AddData
     {
         [DataMember]
@@ -45,6 +71,13 @@ namespace ServiceRecipe
         [DataMember]
         public string Recipe { get; set; }
     }
+
+    /*
+     * กำหนด class ShowAll เพื่อเก็บ FoodID เป็น int , FoodName เป็น string, RawMaterial เป็น string,  Recipe เป็น string 
+     * และสามารถเรียกดูและกำหนดค่าลงไปได้
+     * จะถูกเรียกใช้โดย Service SearchAndUpdate SearchFoodName(string input_foodname); 
+     * และ Service void UpdateDataFood(SearchAndUpdate update_data_food);
+     */
     public class SearchAndUpdate
     {
         [DataMember]
@@ -59,6 +92,12 @@ namespace ServiceRecipe
         [DataMember]
         public string Recipe { get; set; }
     }
+
+    /*
+     * กำหนด class DeleteData เพื่อเก็บ FoodName เป็น string 
+     * และสามารถเรียกดูและกำหนดค่าลงไปได้
+     * จะถูกเรียกใช้โดย Service bool DeleteDataFood(DeleteData delete_data_food);
+     */
     public class DeleteData
     {
         [DataMember]
